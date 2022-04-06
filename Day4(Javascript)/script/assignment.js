4b5f0e37f2bcd1fe9e5d66e387e9ea0433d9efaa
seungwoo lee <timosean@naver.com>
2022-04-06 21:56:32 +0900

    Added Javascript Study before Midterm exam

---
+++ Day4(Javascript)/script/assignment.js
@@ -0,0 +1,9 @@
+document.getElementById('hello').style.fontSize = '16px';
+
+function onAlertAppear() {
+    document.getElementById('alert').style.display='block';
+}
+
+function onAlertDisappear() {
+    document.getElementById('alert').style.display='none';
+}
