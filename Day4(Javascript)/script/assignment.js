9f7573a9074b71232f0045bbf542779b1795066b
seungwoo lee <timosean@naver.com>
2022-05-01 19:06:07 +0900

    midterm assignment 1

--- Day4(Javascript)/script/assignment.js
+++ Day4(Javascript)/script/assignment.js
@@ -1,9 +1,30 @@
-document.getElementById('hello').style.fontSize = '16px';
+document.getElementById("hello").style.fontSize = "16px";
 
 function onAlertAppear() {
-    document.getElementById('alert').style.display='block';
+  const alertBox = document.getElementById("alert");
+  alertBox.style.visibility = "visible";
+  alertBox.style.height = "125px";
+
+  console.log("visible");
 }
 
 function onAlertDisappear() {
-    document.getElementById('alert').style.display='none';
+  const alertBox = document.getElementById("alert");
+  alertBox.style.visibility = "hidden";
+  alertBox.style.height = "0px";
+}
+
+function btnAlert(e) {
+  const goalText = e.target.innerHTML + "를 입력해주세요.";
+  const alertBox = document.getElementById("alert");
+  const alertText = document.getElementById("alert-text");
+  alertBox.style.visibility = "visible";
+  alertBox.style.height = "125px";
+  alertText.innerText = goalText;
+}
+
+for (let i = 0; i < 2; i++) {
+  document
+    .getElementsByClassName("btn-item")
+    [i].addEventListener("click", btnAlert);
 }
